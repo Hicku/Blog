1de7bfd53bb9450d9753ba5da78086d9467c0dca
unknown <aaron.hickman201@gmail.com>
2023-11-06 19:56:05 +0000

    stylw

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,6 +11,52 @@ router.get("/", withAuth, async (req, res) => {
     res.render("homepage")
 })
 
+router.get("/profile/:id", withAuth, async (req, res) => {
+    try {
+        const followee_id = req.params.id;
+        const follower_id = req.params.id;
+        const followingCount = await Follow.count({ where: { follower_id } });
+        const followerCount = await Follow.count({ where: { followee_id } });
+
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.params.id
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"],
+                    include: [{ model: Follow }],
+                },
+                {
+                    model: Likes,
+                },
+                {
+                    model: Comment,
+                    include: [{ model: User }],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render("profile", {
+            followee_id: req.params.id,
+            follower_id: req.session.user_id,
+            user: req.session.user,
+            followerCount,
+            followingCount,
+            posts,
+            isCurrentUser,
+            logged_in: req.session.logged_in,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json(error);
+    }
+});
+
+
 
 router.get("/dashboard", withAuth, async (req, res) => {
     try {
@@ -65,50 +111,6 @@ router.get("/register", (req, res) => {
     res.render("register");
 });
 
-router.get("/profile/:id", withAuth, async (req, res) => {
-    try {
-        const followee_id = req.params.id;
-        const follower_id = req.params.id;
-        const followingCount = await Follow.count({ where: { follower_id } });
-        const followerCount = await Follow.count({ where: { followee_id } });
-
-        const postData = await Post.findAll({
-            where: {
-                user_id: req.params.id
-            },
-            include: [
-                {
-                    model: User,
-                    attributes: ["username"],
-                    include: [{ model: Follow }],
-                },
-                {
-                    model: Likes,
-                },
-                {
-                    model: Comment,
-                    include: [{ model: User }],
-                },
-            ],
-        });
-
-        const posts = postData.map((post) => post.get({ plain: true }));
-
-        res.render("profile", {
-            followee_id: req.params.id,
-            follower_id: req.session.user_id,
-            user: req.session.user,
-            followerCount,
-            followingCount,
-            posts,
-            isCurrentUser,
-            logged_in: req.session.logged_in,
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json(error);
-    }
-});
 
 
 module.exports = router;
